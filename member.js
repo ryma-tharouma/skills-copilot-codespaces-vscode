fef75601dad0a5e9f2d35f8d8ef59cc6b851efc8
Ryma THAROUMA <114444333+ryma-tharouma@users.noreply.github.com>
2024-12-04 18:50:14 +0000

    Copilot second commit 2

--- member.js
+++ member.js
@@ -1,4 +1,4 @@
-function skillMember() {
+function skillsMember() {
     var x = calculateNumbers(1, 2);
     var y = calculateNumbers(1, 2, 3);
     var z = calculateNumbers(1, 2, 3, 4); // will throw an error
